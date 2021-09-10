d37640ccde686c76e5d40611019b3022f9520432
2010262955@qq.com <2010262955@qq.com>
2021-09-10 19:50:40 +0800

    实现组件NavBar

---
+++ src/components/navBar/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="navBar">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"NavBar",
+  components: {},
+  props: {},
+  data() {
+    return {
+    }
+  },
+};
+</script>
+<style lang="less" scoped>
+#navBar{
+    display: flex;
+    // flex-direction: row;
+}
+</style>
---
+++ src/components/navBar/NavBarItem.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="navBarItem">
+        <span class="barItem">
+            <slot></slot>
+        </span>  
+  </div>
+</template>
+
+<script>
+export default {
+  name:"NavBarItem",
+  components: {},
+  props: {},
+  data() {
+    return {
+    }
+  },
+};
+</script>
+<style lang="less" scoped>
+#navBarItem{
+    margin-right: 20px;
+}
+.barItem{
+    display: block;
+    height: 35px;
+    line-height: 35px;
+    // border-bottom: 3px solid #EC4141;
+    // border:1px solid yellow;
+}
+.barItem:hover{
+    // font-size: 18px;
+    font-weight: bold;
+}
+</style>
