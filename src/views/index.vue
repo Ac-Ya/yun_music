037c32270ed9e3cfd76b705b95666b02cae7bbf2
2010262955@qq.com <2010262955@qq.com>
2021-09-10 19:49:13 +0800

    侧边栏的实现

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,26 +1,34 @@
 
 <template>
-  <el-container>
+  <el-container id="index">
     <!-- 头部 -->
     <el-header>
       <header-bar></header-bar>
     </el-header>
     <!-- 内容主体 -->
-    <el-container>
+    <el-container class="center">
       <!-- 侧边栏 -->
-      <el-aside></el-aside>
+      <el-aside width="200px">
+        <aside-nav></aside-nav>
+      </el-aside>
       <!-- 主要内容区 -->
-      <el-main></el-main>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
+    <footer class="musicControl">footer </footer>
   </el-container>
 </template>
 
 <script>
 import HeaderBar from "components/headerBar/HeaderBar";
+import AsideNav from "components/asideNav/AsideNav";
+
 export default {
-  name: "vue",
+  name: "Index",
   components: {
     HeaderBar,
+    AsideNav
   },
   props: {},
   data() {
@@ -37,10 +45,35 @@ export default {
   },
 };
 </script>
-<style scoped>
+<style lang="less" scoped>
+  #index{
+    display: flex;
+    height: 100vh;
+  }
+  .center{
+    flex: 1;
+  }
   .el-header{
     width: 100%;
     padding: 0;
     background-color: #EC4141;
+  }
+   .el-aside {
+    color: #333;
+  }
+  
+  .el-main {
+    // background-color: #E9EEF3;
+    color: #333;
+    // text-align: center;
+    margin:15px 0 0 0;
+    padding: 0;
+    // line-height: 160px;
+    // border: 1px solid red;
+  }
+  .musicControl{
+    width: 100%;
+    height: 80px;
+    background-color: mediumaquamarine;
   }
 </style>
