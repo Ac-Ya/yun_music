8e07778c5922373ac58dfafa741cbc188a9e184b
2010262955@qq.com <2010262955@qq.com>
2021-09-12 11:06:34 +0800

    发现音乐的路由

--- src/network/request.js
+++ src/network/request.js
@@ -5,10 +5,11 @@ export function request(config) {
     let instance = axios.create({
         baseURL: "http://127.0.0.1:3000",
         timeout: 3000,
-        responseType: 'json'
+        responseType: 'json',
+
     })
 
-    //请求拦截器 添加token 保证获取数据的权限
+    // 请求拦截器 添加token 保证获取数据的权限
     // instance.interceptors.request.use(config => {
     //     config.headers.Authorization = window.sessionStorage.getItem("token")
     //     return config
