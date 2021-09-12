312ef76fb75e8478ab3fd4975e85463dc4318ac7
2010262955@qq.com <2010262955@qq.com>
2021-09-12 11:08:53 +0800

    音乐卡片组件封装

---
+++ src/components/cardList/CardList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="cardList">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"CardList",
+  components: {},
+  props: {},
+  data() {
+    return {
+    }
+  },
+};
+</script>
+<style lang="less" scoped>
+#cardList{
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/components/cardList/CardListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="cardListItem">
+    <slot name="item-img"></slot>
+    <slot name="item-playCount"></slot>
+    <slot name="item-playIconfont"></slot>
+    <slot name="item-title"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardListItem",
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="less" scoped>
+#cardListItem{
+      position: relative;
+      // min-width: 150px;
+      // max-width: 200px;
+      margin: 0 10px 20px 0;
+}
+</style>
