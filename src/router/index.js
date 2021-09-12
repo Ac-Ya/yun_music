b147d6f63de32c4bc7274fffbeb561ee3620f373
2010262955@qq.com <2010262955@qq.com>
2021-09-12 22:21:04 +0800

    发现页的歌单

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,18 @@ import VueRouter from 'vue-router'
 const Login = () => import("views/Login.vue")
 const Index = () => import("views/index.vue")
 const DiscoverMusic = () => import("views/discoverMusic/DiscoverMusic.vue")
+const Recommendation = () => import("views/discoverMusic/discoverMusicChild/Recommendation.vue")
+const Playlist = () => import("views/discoverMusic/discoverMusicChild/Playlist.vue")
+const NewMusic = () => import("views/discoverMusic/discoverMusicChild/NewMusic.vue")
+const Singer = () => import("views/discoverMusic/discoverMusicChild/Singer.vue")
+const Top = () => import("views/discoverMusic/discoverMusicChild/Top.vue")
+
+// 解决ElementUI导航栏中的vue-router在3.0版本以上重复点菜单报错问题
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
 
 Vue.use(VueRouter)
 
@@ -12,16 +24,39 @@ const routes = [
   { path: '/', redirect: '/index' },
   {
     path: "/login",
-    component:Login
+    component: Login
   },
   {
     path: '/index',
     component: Index,
-    redirect:"/discoverMusic",
+    redirect: "/discoverMusic",
     children: [
       {
         path: "/discoverMusic",
-        component:DiscoverMusic
+        component: DiscoverMusic,
+        redirect:"/discoverMusic/recommendation",
+        children: [
+          {
+            path: "/discoverMusic/recommendation",
+            component: Recommendation
+          },
+          {
+            path: "/discoverMusic/playlist",
+            component: Playlist
+          },
+          {
+            path: "/discoverMusic/newMusic",
+            component: NewMusic
+          },
+          {
+            path: "/discoverMusic/singer",
+            component: Singer
+          },
+          {
+            path: "/discoverMusic/top",
+            component: Top
+          }
+        ]
       }
     ]
   }
@@ -29,7 +64,7 @@ const routes = [
 
 const router = new VueRouter({
   routes,
-  mode:"history"
+  mode: "history"
 })
 
 export default router
