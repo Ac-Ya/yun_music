d7ea8a3c46db10f1158517d73e93d43fca558116
2010262955@qq.com <2010262955@qq.com>
2021-09-19 23:28:48 +0800

    部分功能修改

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,31 @@
 
 <script>
 
-
+import { request } from "network/request.js";
 export default {
+
   name: 'app',
   components: {
-
+  },
+  created(){
+    let store = this.$store
+    let musicListId =  window.localStorage.getItem("musicListId")
+    store.commit("modifyMusicId",window.localStorage.getItem('currentMusic'))
+  },
+  methods:{
+    async getMusicListdata(musicListId){
+      let res = await request({
+        url:''
+      })
+    }
+  },
+  beforeDestroy(){
+    let state = this.$store.state
+    // console.log(2);
+    // 获取vuex中currentMusic 和 musicListId
+    window.localStorage.setItem("currentMusic",state.currentMusic)
+    window.localStorage.setItem("musicListId",state.musicListId)
+    state = null
   }
 }
 </script>
@@ -20,4 +40,5 @@ export default {
     width: 100%;
     height: 100%;
   }
+  
 </style>
