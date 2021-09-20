b5943e68958f68d8e55c694c802e6121ca318e92
2010262955@qq.com <2010262955@qq.com>
2021-09-20 23:19:42 +0800

    实现歌词滚动

--- src/components/lyricsScroll/LyricsScroll.vue
+++ src/components/lyricsScroll/LyricsScroll.vue
@@ -1,10 +1,20 @@
 <template>
   <div id="lyricsScroll">
-    <div class="lyricsItem" v-for="(item) in lyrics" :key="item[0]">{{item[1]}}</div>
+    <div class="placeholder"></div>
+    <div
+      class="lyricsItem"
+      :class="lyricsIndex - 1 == index ? 'currentLyric' : ''"
+      v-for="(item, index) in lyrics"
+      :key="index"
+    >
+      {{ item[1] }}
+    </div>
+    <!-- <div class="placeholder"></div> -->
   </div>
 </template>
 
 <script>
+let placeholderHeight = 0;
 export default {
   name: "LyricsScroll",
   components: {},
@@ -18,30 +28,66 @@ export default {
     return {
       // 当前歌词索引
       lyricsIndex: 0,
+      currentPlayTime: 0,
     };
   },
+  mounted() {},
   methods: {
     // 实现歌词滚动
-    // lyricScroll(currentLyric) {
-    //   // 获取歌词item
-    //   let lyricsArr = document.querySelectorAll(".lyricsItem");
-    //   // 获取歌词框
-    //   let lyrics = document.getElementById("lyricsScroll");
-    //   // placeholder的高度
-    //   if (placeholderHeight == 0) {
-    //     placeholderHeight = lyricsArr[0].offsetTop - lyrics.offsetTop;
-    //   }
-    //   //   歌词item在歌词框的高度 = 歌词框的offsetTop - 歌词item的offsetTop
-    //   //   console.log(currentLyric);
-    //   if (lyricsArr[currentLyric - 1]) {
-    //     let distance = lyricsArr[currentLyric - 1].offsetTop - lyrics.offsetTop;
-    //     //   lyricsArr[currentLyric].scrollIntoView();
-    //     lyrics.scrollTo({
-    //       behavior: "smooth",
-    //       top: distance - placeholderHeight,
-    //     });
-    //   }
-    // },
+    lyricScroll(currentLyric) {
+      // 获取歌词item
+      let lyricsArr = document.querySelectorAll(".lyricsItem");
+      // 获取歌词框
+      let lyric = document.getElementById("lyricsScroll");
+      //   歌词item在歌词框的高度 = 歌词框的offsetTop - 歌词item的offsetTop
+      if (lyricsArr[currentLyric]) {
+        let distance = lyricsArr[currentLyric].offsetTop - lyric.offsetTop *2;
+        // console.log(lyricsArr[currentLyric].offsetTop);
+        lyric.scrollTo({
+          behavior: "smooth",
+          top: distance,
+        });
+      }
+    },
+    getCurrentLyricsIndex(currentTime) {
+      let lyricsIndex = 0;
+      this.lyrics.some((item) => {
+        if (lyricsIndex < this.lyrics.length - 1) {
+          if (currentTime > item[0]) {
+            lyricsIndex += 1;
+          }
+          return currentTime <= item[0];
+        }
+      });
+      // console.log(lyricsIndex);
+      this.lyricsIndex = lyricsIndex;
+    },
+  },
+  watch: {
+    //监听当前歌曲播放时长
+    "$store.state.currentPlayTime"(currentPlayTime, lastTime) {
+      // console.log(currentPlayTime,lastTime);
+      this.currentPlayTime = currentPlayTime;
+      this.getCurrentLyricsIndex(currentPlayTime);
+      if (
+        (lastTime && Math.abs(currentPlayTime - lastTime) >= 1) ||
+        (this.lyricsIndex == 0 && this.lyrics.length > 1)
+      ) {
+        // 处理播放时间跳转时歌词位置的校准
+        if (this.lyrics.length > 1) {
+          this.getCurrentLyricsIndex(currentPlayTime);
+          // 滑动到当前歌词
+          this.lyricScroll(this.lyricsIndex);
+        }
+      }
+      // 根据实时播放时间实现歌词滚动
+      // console.log(currentPlayTime,this.lyrics[this.lyricsIndex][0]);
+      if (this.lyricsIndex < this.lyrics.length) {
+        if (currentPlayTime <= this.lyrics[this.lyricsIndex][0]) {
+          this.lyricScroll(this.lyricsIndex);
+        }
+      }
+    },
   },
 };
 </script>
@@ -57,8 +103,19 @@ export default {
 }
 .lyricsItem {
   width: 100%;
-  height: 35px;
+  height: 25px;
   text-align: center;
-  color: #dcdcdc;
+  color: #696969;
+  margin-bottom: 15px;
+  font-size: 16px;
+}
+.currentLyric {
+  font-weight: 600;
+  font-size: 18px;
+  color: black;
+}
+.placeholder {
+  width: 100%;
+  height: 30px;
 }
 </style>
