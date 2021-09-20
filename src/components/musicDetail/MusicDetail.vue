1527310dfded049b69723e29e6f8158f9ff2d5f2
2010262955@qq.com <2010262955@qq.com>
2021-09-20 23:20:19 +0800

    歌曲详情页的动态显示

--- src/components/musicDetail/MusicDetail.vue
+++ src/components/musicDetail/MusicDetail.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="musicDetail">
-    <div class="inner" :class="{hide:isShowDetail}">
-      <div class="bgImg"></div>
+    <div class="inner" v-if="musicDetail !== null">
+      <div class="bgImg" ></div>
       <!-- 音乐详情头部 -->
       <div class="header">
         <i class="iconfont icon-arrow_down" @click="showPlay"></i>
@@ -21,20 +21,30 @@
               :class="{ aninationRunning: play, animationPause: !play }"
             >
               <img src="../../assets/img/disc.png" alt="" class="bDisk" />
-              <img src="../../assets/img/tx.png" alt="" class="bg" />
+              <img :src="musicDetail.al.picUrl" alt="" class="bg" />
             </div>
           </div>
         </div>
         <!-- 歌名和歌手名 -->
         <div class="musicName">
-          <span class="name">银河与星斗</span>
-          <span class="singer">yihuik苡慧 - 银河与星斗</span>
+          <span class="name">{{musicDetail.al.name}}</span>
+          <span class="singer">{{musicDetail.ar[0].name}}</span>
         </div>
         <!-- 歌词区域 -->
         <lyrics-scroll class="lyricsScroll" :lyrics="lyric"></lyrics-scroll>
         <!-- 评论区域 -->
-        <comment class="comment"></comment>
+        <comment
+          class="comment"
+          :commentType="+0"
+          :sourceID="+this.currentMusicId"
+        ></comment>
       </div>
+    </div>
+    <div class="inner2" v-else>
+      <div class="header">
+        <i class="iconfont icon-arrow_down" @click="showPlay"></i>
+      </div>
+      <span class="message">对不起暂时没有播放的音乐！</span>
     </div>
   </div>
 </template>
@@ -51,72 +61,102 @@ export default {
   data() {
     return {
       play: false,
-      isShowDetail:false,
-      lyric:[],//用于保存歌词
+      isShowDetail: false,
+      lyric: [], //用于保存歌词
+      currentMusicId: 0, //用于保存音乐id
+      musicDetail: null,
     };
   },
-  created() {
-    this.getLyric()
-  },
+  created() {},
   methods: {
     showPlay() {
-      this.isShowDetail = false
-      this.$store.commit("modifyShowDetail",false)
-      
+      this.isShowDetail = false;
+      this.$store.commit("modifyShowDetail", false);
     },
     //获取歌词
-    async getLyric() {
+    async getLyric(id) {
       let res = await request({
-        url:"/lyric",
-        method:"get",
-        params:{
-          id:1846292840
-        }
+        url: "/lyric",
+        method: "get",
+        params: {
+          id,
+        },
       });
-        // console.log(res);
-      let lyrics = res.data.lrc.lyric;
-      // 对获取到的歌词进行处理
-      let arr = lyrics.split("\n");
-      let array = [];
-      let time = "";
-      let value = "";
-      let result = [];
+      console.log(res);
+      if( res.data.lrc){
+        let lyrics = res.data.lrc.lyric;
+        // 对获取到的歌词进行处理
+        let arr = lyrics.split("\n");
+        let array = [];
+        let time = "";
+        let value = "";
+        let result = [];
 
-      //去除空行
-      arr.forEach((item) => {
-        if (item != "") {
-          array.push(item);
-        }
-      });
-      arr = array;
-      arr.forEach((item) => {
-        time = item.split("]")[0];
-        value = item.split("]")[1];
-        //去掉时间里的中括号得到xx:xx.xx
-        var t = time.slice(1).split(":");
-        //将结果压入最终数组
-        result.push([parseInt(t[0], 10) * 60 + parseFloat(t[1]), value]);
-      });
+        //去除空行
+        arr.forEach((item) => {
+          if (item != "") {
+            array.push(item);
+          }
+        });
+        arr = array;
+        arr.forEach((item) => {
+          time = item.split("]")[0];
+          value = item.split("]")[1];
+          //去掉时间里的中括号得到xx:xx.xx
+          var t = time.slice(1).split(":");
+          //将结果压入最终数组
+          result.push([parseInt(t[0], 10) * 60 + parseFloat(t[1]), value]);
+        });
+        this.lyric = result;
+        arr = null,time = null,value= null, result = null
+      }else {
+        this.lyric = [[0,"纯音乐请享受！"]];
+      }  
 
-      this.lyric = result;
       // console.log(result);
     },
+    //获取音乐信息
+    async getMusicDetail(ids) {
+      let res = await request({
+        url: "/song/detail",
+        method: "get",
+        params: {
+          ids,
+        },
+      });
+      // console.log(res);
+      this.musicDetail = res.data.songs[0];
+    },
   },
-  watch:{
-    "$store.state.showDetail"(showDetail){
-      console.log(showDetail);
+  watch: {
+    "$store.state.showDetail"(showDetail) {
+      let currentMusicId = this.currentMusicId;
       //修改是否显示歌曲详情
-      this.isShowDetail = showDetail
+      this.isShowDetail = showDetail;
+      if (showDetail && currentMusicId ) {
+        this.getLyric(currentMusicId);
+        this.getMusicDetail(currentMusicId);
+      }
+    },
+    "$store.state.currentMusic"(currentMusic) {
+      this.currentMusicId = currentMusic.musicId;
+      if (this.isShowDetail && this.currentMusicId ) {
+        this.getLyric(currentMusic.musicId);
+        this.getMusicDetail(currentMusic.musicId);
+      }
+    },
+    "$store.state.isPlay"(isPlay){
+      this.play = isPlay
     }
-  }
+  },
 };
 </script>
 <style lang="less" scoped>
 .inner {
   position: relative;
   width: 100%;
-  height: 0;
-  transition: height 0.5s linear   ;
+  height: 100%;
+  transition: height 0.5s linear;
   .bgImg {
     position: absolute;
     z-index: -1;
@@ -124,24 +164,20 @@ export default {
     left: 0;
     width: 100%;
     height: 100%;
-    background-image: url("../../assets/img/tx.png");
+    // background-image: url("../../assets/img/tx.png");
+    background: linear-gradient(45deg,rgba(254,172,94,0.5),rgba(199,121,208,0.5),rgba(75,192,200,0.5));
+
     background-repeat: no-repeat;
     background-size: cover;
     -webkit-background-size: cover;
     -o-background-size: cover;
     background-position: center 0;
     transform: scale(1.2);
-    filter: blur(30px) brightness(0.6) drop-shadow(-10px 0 20px #000);
+    filter:  brightness(0.8);
+    // filter: blur(30px) brightness(0.8) drop-shadow(-10px 0 20px #000);
+
   }
-  .header {
-    position: absolute;
-    left: 20px;
-    top: 20px;
-    color: white;
-    .iconfont {
-      font-size: 30px;
-    }
-  }
+
   .musicInfo {
     position: relative;
     width: 1400px;
@@ -152,25 +188,46 @@ export default {
     bottom: 0;
     margin: auto;
     overflow-y: scroll;
-    &::-webkit-scrollbar{
+    &::-webkit-scrollbar {
       display: none;
     }
-    .track{
+    .track {
       position: absolute;
       left: 200px;
       top: 20px;
     }
-    .lyricsScroll{
+    .lyricsScroll {
       position: absolute;
     }
-  
+  }
+}
+.header {
+  position: absolute;
+  left: 20px;
+  top: 20px;
+  color: white;
+  .iconfont {
+    font-size: 30px;
+  }
+}
+.inner2 {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transition: height 0.5s linear;
+  background-color: #dcdcdc;
+  .message {
+    position: absolute;
+    height: 100%;
+    top: 80%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: black;
+    font-size: 30px;
+    font-weight: bold;
   }
 }
 
-.hide{
-  height: calc(100vh - 70px);
-  transition: height 0.5s linear   ;
-}
 .disk {
   position: absolute;
   top: 150px;
