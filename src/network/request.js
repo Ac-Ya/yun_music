0056f0d139dbf9f0c56175195d976ace9faebd29
2010262955@qq.com <2010262955@qq.com>
2021-09-22 22:41:43 +0800

    修改请求

--- src/network/request.js
+++ src/network/request.js
@@ -1,11 +1,11 @@
 // 封装axios请求
 const axios = require("axios")
 
+axios.defaults.withCredentials=true;
 export function request(config) {
     let instance = axios.create({
         baseURL: "http://127.0.0.1:3000",
-        timeout: 3000,
-        responseType: 'json',
+        timeout: 20000,
         withCredentials: true
     })
 
@@ -16,26 +16,28 @@ export function request(config) {
     // })
 
     instance.interceptors.request.use(config => {
+        // console.log(document.cookie);
         // console.log(config);
-        // config.headers = window.localStorage.getItem('cookie')
+        // config.Cookie = document.cookie
+        config.credentials  = true
         return config
     }, err => {
         console.log(err);
     });
 
-    instance.interceptors.response.use(config => {
-        // console.log(config);
-        return config;
-    }, err => {
-        console.log([err]);
-        if (err.response.data.msg == '需要登录') {
+    // instance.interceptors.response.use(config => {
+    //     // console.log(config);
+    //     return config;
+    // }, err => {
+    //     console.log([err]);
+    //     if (err.response.data.msg == '需要登录') {
            
-            console.log(err.response.data.msg);
+    //         // console.log(err.response.data.msg);
 
-        } else {
-            console.log(err.response.message);
-        }
-    });
-    instance.defaults.withCredentials = true;
+    //     } else {
+    //         console.log(err.response.message);
+    //     }
+    // });
+    // instance.defaults.withCredentials = true;
     return instance(config) 
 }
