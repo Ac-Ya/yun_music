c4226143913ec7c25f4a7c8454c7816022a9112a
2010262955@qq.com <2010262955@qq.com>
2021-09-26 23:22:48 +0800

    新增工具函数

--- src/plugins/utils.js
+++ src/plugins/utils.js
@@ -1,12 +1,11 @@
-const formatTime = date => {
+const formatTime = (date,isHMS=true) => {
     const year = date.getFullYear()
     const month = date.getMonth() + 1
     const day = date.getDate()
     const hour = date.getHours()
     const minute = date.getMinutes()
     const second = date.getSeconds()
-  
-    return `${[year, month, day].map(formatNumber).join('-')} ${[hour, minute, second].map(formatNumber).join(':')}`
+    return isHMS ? `${[year, month, day].map(formatNumber).join('-')} ${[hour, minute, second].map(formatNumber).join(':')}`: `${[year, month, day].map(formatNumber).join('-')}`
 }
   
 
