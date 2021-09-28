169379b83a5f732aa096488ed2458f1247119302
2010262955@qq.com <2010262955@qq.com>
2021-09-28 22:35:46 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -9,11 +9,19 @@ import "./assets/css/base.css"
 import "./assets/iconfontCss/iconfont.css"
 import 'element-ui/lib/theme-chalk/index.css';
 import less from 'less'
+// import VueLazyLoad from "vue-lazyload"
+
 // event-bus.js
 
 Vue.prototype.$EventBus = new Vue()
 
-
+// Vue.use(VueLazyload,{
+//   preload:1.3,//预加载的宽高
+//   loading:require("./assets/img/loadError.gif"),
+//   error:"./assets/img/loadError.gif",
+//   attempt:3,//尝试加载的次数
+//   listenEvents:['scroll','wheel','mousewheel','resize','animationend','transitionend','touchmove'], //你想让vue监听的事件
+// })
 Vue.use(less)
 
 Vue.config.productionTip = false
